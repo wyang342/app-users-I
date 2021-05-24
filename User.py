94b526c60f1cf2a4dc1fbfe3012a0e344e154df9
Won Yang <won@gatech.edu>
2021-05-24 13:37:13 -0500

    adds solution

--- User.py
+++ User.py
@@ -1,1 +1,20 @@
 # your User class goes here
+class User:
+    name = "Bob"
+    email_address = "bob@google.com"
+    license_number = 123123123
+
+    def __init__(self, name, email_address, license_number):
+        self.name = name
+        self.email_address = email_address
+        self.license_number = license_number
+
+    def interact(self):
+        return "interacted!"
+
+    def __str__(self):
+        return "I am {}, my email is {}, and my license number is {}.".format(self.name, self.email_address, self.license_number)
+
+
+rob = User("Rob", "rob@gmail.com", 123321123)
+print(rob)
